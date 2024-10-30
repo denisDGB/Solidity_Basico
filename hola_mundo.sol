ba2c59cfcb4c4895fa1fa442005289f5151d5b79
denisDGB <denis.gomez171997@gmail.com>
2024-10-30 12:34:16 -0600

    Primer contrato

---
+++ hola_mundo.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// 0x0B6973Fad1888117E6255281BAf3F4AB060fE3dA
+
+pragma solidity 0.8.24; // en esta parte defino la version del compilador
+
+// A partir de aca coloco la logica de mi contrato
+contract hola_mundo {
+   
+    string public saludo = "Hola desde Educateth"; // declarando el contenido de mi variable saludo
+
+}
